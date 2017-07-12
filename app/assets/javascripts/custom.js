40711dbd031af1cf43add78ccc3c36a8defcc7d4
ChanneDesign <channebertrand@gmail.com>
2017-07-12 15:56:26 -0400

    css fix margin top home

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -42,8 +42,8 @@ $(document).on('turbolinks:load', function() {
         $(bgs).addClass("parallax-bg-" + num)
         var anchor = $(bgs).parent().addClass("parallax-bg-anchor" + num)
         console.log($(bgs).parent())
-        var parallax = $tm.to($(".parallax-bg-" + num), 1, { y: '50%', ease: Power1.easeOut });
-        var slideParallaxScene = new ScrollMagic.Scene({ triggerElement: ".parallax-bg-anchor" + num, duration: "100%", triggerHook: 0 })
+        var parallax = $tm.to($(".parallax-bg-" + num), 1, { y: '50%', ease: Power2.easeOut });
+        var slideParallaxScene = new ScrollMagic.Scene({ triggerElement: ".parallax-bg-anchor" + num, duration: "100%", triggerHook: 0.2 })
                   .setTween(parallax)
                   .addTo(animController);
       });
@@ -66,66 +66,66 @@ $(document).on('turbolinks:load', function() {
     // }
   })();
   // MouseWheel SmoothScroll
-  (function(){
-
-				var $window = $(window);
-				var isTweening = false;
-
-				document.onmousewheel = function(){ customScroll(); }
-				if(document.addEventListener){
-				    document.addEventListener('DOMMouseScroll', customScroll, false);
-				}
-
-				function customScroll(event){
-
-				   var delta = 0;
-
-				   if (!event){
-					   event = window.event;
-				   }
-
-				   if (event.wheelDelta) {
-					   delta = event.wheelDelta/120;
-				   } else if(event.detail) {
-					   delta = -event.detail/3;
-				   }
-
-				   if (delta){
-
-				   		//console.log(isTweening);
-
-				   		//if(!isTweening){
-
-					   		//isTweening = true;
-
-						   	var scrollTop = $window.scrollTop();
-						   	var finScroll = scrollTop - parseInt(delta*100) * 3;
-
-						   	//console.log(finScroll);
-
-						   	TweenMax.to($window, 1, {
-							   	scrollTo : { y: finScroll, autoKill:true },
-							   	ease: Power4.easeOut,
-							   	autoKill: true,
-							   	overwrite: 5,
-							   	onComplete: function(){
-							   		//console.log(isTweening);
-							   		//isTweening = false;
-							   	}
-							});
-
-					   //}
-				   }
-
-				   if (event.preventDefault){
-					   event.preventDefault();
-				   }
-
-				   event.returnValue = false;
-
-				}
-
-			});
+  // (function(){
+  //
+	// 			var $window = $(window);
+	// 			var isTweening = false;
+  //
+	// 			document.onmousewheel = function(){ customScroll(); }
+	// 			if(document.addEventListener){
+	// 			    document.addEventListener('DOMMouseScroll', customScroll, false);
+	// 			}
+  //
+	// 			function customScroll(event){
+  //
+	// 			   var delta = 0;
+  //
+	// 			   if (!event){
+	// 				   event = window.event;
+	// 			   }
+  //
+	// 			   if (event.wheelDelta) {
+	// 				   delta = event.wheelDelta/120;
+	// 			   } else if(event.detail) {
+	// 				   delta = -event.detail/3;
+	// 			   }
+  //
+	// 			   if (delta){
+  //
+	// 			   		//console.log(isTweening);
+  //
+	// 			   		//if(!isTweening){
+  //
+	// 				   		//isTweening = true;
+  //
+	// 					   	var scrollTop = $window.scrollTop();
+	// 					   	var finScroll = scrollTop - parseInt(delta*100) * 3;
+  //
+	// 					   	//console.log(finScroll);
+  //
+	// 					   	TweenMax.to($window, 0.7, {
+	// 						   	scrollTo : { y: finScroll, autoKill:true },
+	// 						   	ease: Power4.easeOut,
+	// 						   	autoKill: true,
+	// 						   	overwrite: 5,
+	// 						   	onComplete: function(){
+	// 						   		//console.log(isTweening);
+	// 						   		//isTweening = false;
+	// 						   	}
+	// 						});
+  //
+	// 				   //}
+	// 			   }
+  //
+	// 			   if (event.preventDefault){
+	// 				   event.preventDefault();
+	// 			   }
+  //
+	// 			   event.returnValue = false;
+  //
+	// 			}
+  //
+	// 		});
   // Hover over effect collections
   (function() {
     var imgMaskTLIn = new TimelineLite();
